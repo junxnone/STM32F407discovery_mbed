141d19525bef556d63343b29f994c5bfc0b4b992
52osworld <junchen0102@gmail.com>
2015-01-27 18:29:43 +0800

    Signed-off-by: 52osworld <junchen0102@gmail.com> 移植OK
    1.可使用DigitalOut 点亮LED

---
+++ User/src/Test.cpp
@@ -0,0 +1,19 @@
+/**
+* @file Test.cpp
+* @brief ²âÊÔÓÃÀı
+*
+*
+*
+*/
+#include "mbed.h"
+#include "Test.h"
+DigitalOut myled6(LED6);
+void Test()
+{
+		myled6=!myled6;
+
+
+}
+
+
+
---
+++ User/src/Test.h
@@ -0,0 +1,24 @@
+/**
+*	@file Test.h
+* @brief ²âÊÔÓÃÀı
+*
+*/
+
+#ifndef ST_TEST_H
+#define ST_TEST_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void Test();
+
+
+	
+	
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
